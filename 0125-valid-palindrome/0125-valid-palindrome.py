6d38b57287435462999ce7277c98939751ebbdc0
Saksham Sharma <Saksham8740@outlook.com>
2026-07-04 12:31:24 +0530

    Time: 17 ms (6.87%), Space: 26.2 MB (5.4%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,12 +1,26 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         t=''
-        for i in s:
-            if i.isalnum():
-                t=t+i.lower()
-       # print(t)
-#This is a brute force method        
-        for i in range(len(t)//2):
-            if t[i]!=t[len(t)-1-i]:
+        for j in s:
+            if j.isalnum():
+                t=t+j.lower()
+# this is a recursive solution 
+        def palindrome(i: int,k: str):
+            if i>=len(k)//2:
+                return True
+            if k[i]!=k[len(k)-1-i]:
                 return False
-        return True
+            else:
+                return palindrome(i+1,k)
+        return palindrome(0,t)
+       # print(t)
+# #This is a brute force method        
+#         for i in range(len(t)//2):
+#             if t[i]!=t[len(t)-1-i]:
+#                 return False
+#         return True
+
+#     #
+        
+
+      
