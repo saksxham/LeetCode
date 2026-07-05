20b7c5ac1ce8daee647bbf9f140013613f71802d
Saksham Sharma <Saksham8740@outlook.com>
2026-07-05 10:29:30 +0530

    Time: 7 ms (36.31%), Space: 20.8 MB (7.31%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,8 +1,27 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-#Brute force method
+#Another method we can use is hashing
+#store elemnts in hash #dictionary in the python
+#iterate through hash , if target-hash present then return the index
+#hash[key,val]#Key:Value in the array num #val:Index of that value in array num
+        hash=dict()
         for i in range(len(nums)):
-            for j in range(i+1,len(nums)):
-                if nums[i]+nums[j] == target:
-                    return [i,j]
+            hash[nums[i]]=i
+        print(hash)
+
+        for i in range(len(nums)):
+            complement=target-nums[i]
+            if complement in hash and hash[complement] != i:
+                return [i,hash[complement]]
+        return []
+        
+#Brute force method
+        # for i in range(len(nums)):
+        #     for j in range(i+1,len(nums)):
+        #         if nums[i]+nums[j] == target:
+        #             return [i,j]
+
+    
+
+    
         
