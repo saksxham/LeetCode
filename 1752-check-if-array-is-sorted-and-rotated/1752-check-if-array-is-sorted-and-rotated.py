e32aecf0088d38903626e298bb93282a7dd612b5
Saksham Sharma <Saksham8740@outlook.com>
2026-07-25 12:57:23 +0530

    Time: 0 ms (100%), Space: 19.3 MB (51.54%) - LeetHub

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.py
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.py
@@ -4,19 +4,42 @@ class Solution:
         #Check for all the rotation array, if its sorted or not
         #an array of size n if rotated n times, becomes same array
         #so the total possible combination is 0,n-1 rotations
-        l=len(nums)
+        # l=len(nums)
 
-        for i in range(l):
-            new_arr=[]
-            for j in range(i,l):
-                new_arr.append(nums[j])
-            for j in range(i):
-                new_arr.append(nums[j])
-            is_sorted=True
-            for k in range(1,l):
-                if new_arr[k]<new_arr[k-1]:
-                    is_sorted=False
-                    break
-            if is_sorted:
-                return True
-        return False
+        # for i in range(l):
+        #     new_arr=[]
+        #     for j in range(i,l):
+        #         new_arr.append(nums[j])
+        #     for j in range(i):
+        #         new_arr.append(nums[j])
+        #     is_sorted=True
+        #     for k in range(1,l):
+        #         if new_arr[k]<new_arr[k-1]:
+        #             is_sorted=False
+        #             break
+        #     if is_sorted:
+        #         return True
+        # return False
+
+
+        #Optimal - Better solution
+        #In a sorted array, either rotated or not, there can be atmost 1 invesrion point
+        # i.e the point where prev element is more than next element
+        #the inversion happens at the min elemt of the array
+        #if there are more than 1 inversion points in the array, this means any number of the rotation cant make it a sorted array
+
+        inversion_point=0
+        for i in range(1,len(nums)):
+            if nums[i]<nums[i-1]:
+                inversion_point=inversion_point+1
+        # also Check for 1st and last
+        if nums[0] < nums[len(nums) - 1]:
+            inversion_point=inversion_point+1
+
+
+        if inversion_point>1:
+            return False
+        else:
+            return True
+
+
