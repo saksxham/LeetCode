8110325c01b589573818412c2ccd933b16869ac1
Saksham Sharma <Saksham8740@outlook.com>
2026-07-25 14:51:56 +0530

    Time: 6 ms (56.94%), Space: 26.4 MB (93.29%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -1,7 +1,9 @@
 class Solution:
     def rotate(self, nums: List[int], k: int) -> None:
+        #Optimal Solution not best
         new_arr=[]
         l=len(nums)
+        #mod of k is taken as array roated its len number of time is exactly same
         k=k%l
         for i in range(l-1-k+1,l):
             new_arr.append(nums[i])
