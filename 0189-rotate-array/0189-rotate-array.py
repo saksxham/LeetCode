ecd34e6838d074bda816857068c6049722f7b15a
Saksham Sharma <Saksham8740@outlook.com>
2026-07-25 15:51:06 +0530

    Time: 10 ms (22.15%), Space: 26.6 MB (43.98%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -1,15 +1,43 @@
 class Solution:
     def rotate(self, nums: List[int], k: int) -> None:
-        #Optimal Solution not best
-        new_arr=[]
         l=len(nums)
-        #mod of k is taken as array roated its len number of time is exactly same
         k=k%l
-        for i in range(l-1-k+1,l):
-            new_arr.append(nums[i])
-        for i in range(l-1-k+1):
-            new_arr.append(nums[i])
-        for i in range(l):
-            nums[i]=new_arr[i]
+        self.reverse(0,l-k-1,nums)
+        self.reverse(l-k,l-1,nums)
+        self.reverse(0,l-1,nums)
+    def reverse(self,i,j,nums):
+        while i<j:
+            temp=nums[i]
+            nums[i]=nums[j]
+            nums[j]=temp
+            i=i+1
+            j=j-1
+# K=3
+
+# l=len(nums)
+
+# reverse(0,l-k,nums)
+# reverse(l-k,l-1,nums)
+# reverse(0,l-1,nums)
+
+# [1,2,3,4-5,6,7]
+
+# 4,3,2,1-7,6,5
+# 5,6,7-1,2,3,4
+
+
+[5,6,7,1,2,3,4]
+
+        # #Optimal Solution not best
+        # new_arr=[]
+        # l=len(nums)
+        # #mod of k is taken as array roated its len number of time is exactly same
+        # k=k%l
+        # for i in range(l-1-k+1,l):
+        #     new_arr.append(nums[i])
+        # for i in range(l-1-k+1):
+        #     new_arr.append(nums[i])
+        # for i in range(l):
+        #     nums[i]=new_arr[i]
 
         
